2f0a8a17ae3b8ff7dd2953e7dc53a77f1c066cbc
jramosp28 <javierrampob@campus.monlau.com>
2023-03-28 18:42:41 +0200

    actualizado

--- src/com/ramos/serializacion/nativa/Ejemplo1.java
+++ src/com/ramos/serializacion/nativa/Ejemplo1.java
@@ -1,10 +1,10 @@
-
 package com.ramos.serializacion.nativa;
 
 import com.ramos.serializacion.model.Product;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 /**
@@ -19,77 +19,39 @@ public class Ejemplo1 {
         System.out.println(prod.toString());
         System.out.println("Vamos a serializar el producto anterior");
         serializar(prod);
-//        Product newProd = null;
-//        newProd = serializar();
-//        System.out.println(newProd.toString());
 
+        Product prodDeserializado = deserializar();
+        System.out.println("Objeto deserializado: " + prodDeserializado);
     }
-//    
-//    static void deserializar(Product p) {
-//
-//        FileOutputStream fileIn = null;
-//
-//        try {
-//
-//            fileIn = new FileOutputStream(".\\Product.ser");
-//            ObjectOutputStream out = new ObjectOutputStream(fileIn);
-//            out.writeObject(p);
-//            System.out.println("Vamos a serializar  elproducto anterior");
-//
-//        } catch (FileNotFoundException ex) {
-//
-//            System.out.println("No se encuentra la ruta del fichero. " + ex.getMessage());
-//
-//        } catch (IOException ex) {
-//
-//            System.out.println("Ha surgido un error serializando. " + ex.getMessage());
-//
-//        } finally {
-//
-//            try {
-//                fileIn.close();
-//
-//            } catch (IOException ex) {
-//
-//                System.out.println("Ha surgido un error cerrando el fichero. " + ex.getMessage());
-//            }
-//
-//        }
-//
-//    }
-//    
-//    ----------------------------------------
 
-    static void serializar(Product p) {
-
-        FileOutputStream fileOut = null;
-
+    public static Product deserializar() {
+        Product prod = null;
         try {
-
-            fileOut = new FileOutputStream(".\\Product.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(p);
-            System.out.println("Hemos serializado en el fichero Product.ser");
-
-        } catch (FileNotFoundException ex) {
-
-            System.out.println("No se encuentra la ruta del fichero. " + ex.getMessage());
-
-        } catch (IOException ex) {
-
-            System.out.println("Ha surgido un error serializando. " + ex.getMessage());
-
-        } finally {
-
-            try {
-                fileOut.close();
-
-            } catch (IOException ex) {
-
-                System.out.println("Ha surgido un error cerrando el fichero. " + ex.getMessage());
-            }
-
+            FileInputStream fileIn = new FileInputStream("producto.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            prod = (Product) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("La clase Product no ha sido encontrada.");
+            c.printStackTrace();
         }
+        return prod;
+    }
 
+//    ----------------------------------------
+    public static void serializar(Product prod) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("producto.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(prod);
+            out.close();
+            fileOut.close();
+            System.out.println("Objeto serializado y almacenado en 'producto.ser'");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
     }
 }
